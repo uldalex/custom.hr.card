cdfb3945aca08808bfc46c84766823350056f996
Юлдашева Александра <a.juldasheva@qwell.ru>
2026-01-26 11:49:10 +0300

    Каркас админ-интерфейса

--- admin/fields.php
+++ admin/fields.php
@@ -1,1 +1,18 @@
 <?
+// Подключение Bitrix пролога для admin
+require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_admin_before.php');
+
+// Подключение нашего модуля
+use Bitrix\Main\Loader;
+
+Loader::includeModule('custom.hr.card'); //  код модуля
+$APPLICATION->SetTitle("Custom HR Card Admin");
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_admin_after.php');
+?>
+
+<h1>Custom HR Card</h1>
+<p>Модуль установлен</p>
+
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/epilog_admin.php');
